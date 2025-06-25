11c8880342b0e9d4fc72da304c1fc0468824d568
PrasunPrabhat <prasun2000kir@gmail.com>
2025-06-25 19:20:04 +0530

    Issue some

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "https://cinescope-ai.onrender.com",
-  "https://netflix-frontend-d318e.web.app",
   "https://cine-scope-ai-prompt-based-movie-re.vercel.app/",
   "http://localhost:3000", // optional, for local development
 ];
